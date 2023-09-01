a87eb1bdf8273f4d9b90aadf74337c93840cc741
Frank Dersjant <36342252+frankdersjant@users.noreply.github.com>
2023-09-01 12:45:12 +0200

    modified seri

--- Seri/Function1.cs
+++ Seri/Function1.cs
@@ -1,7 +1,9 @@
 using System.Net;
+using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Seri.Models;
 
 namespace Seri
 {
@@ -14,15 +16,22 @@ namespace Seri
             _logger = loggerFactory.CreateLogger<Function1>();
         }
 
-        [Function("Function1")]
+        [Function("Seri")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string requestBody = String.Empty;
+
+            using (StreamReader streamReader = new StreamReader(req.Body))
+            {
+                requestBody = streamReader.ReadToEnd();
+            }
+
+            Customer customerdata = JsonSerializer.Deserialize<Customer>(requestBody);
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString(customerdata.Name + DateTime.Now);
 
             return response;
         }
